c4882e800d7c279ed746577f0b384bdbac486588
stefi23 <stefania.rosca2309@gmail.com>
2020-05-07 16:26:42 +0200

    refactored computer Random

--- src/tic-tac-toe-computerRandom.js
+++ src/tic-tac-toe-computerRandom.js
@@ -1,157 +1,59 @@
-const positionsBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+const Board = require("./board");
+const GameUI = require("./gameUI");
+const board = new Board();
+const gameUI = new GameUI(board);
 
-//This is a const that allows user to add input
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let board;
-let xTurn = true;
-let computerTurn = false;
-
-console.log("Welcome to the game!");
-
-render(positionsBoard);
-
-initializaGame();
-
-play(xTurn, computerTurn);
-
-//Functions:
-
-function initializaGame() {
-  board = ["", "", "", "", "", "", "", "", ""];
-}
-
-function render(board) {
-  console.log(board.slice(0, 3));
-  console.log(board.slice(3, 6));
-  console.log(board.slice(6, 9));
-}
-
-function play(xTurn, computerTurn) {
-  if (getWinner() === "tie") {
-    return readline.close();
-  }
-  if (xTurn) {
-    readline.question(`What's  your  move X?`, (position) => {
-      position = parseInt(position);
-      if (!isInputCorrect(position)) {
-        play(xTurn, computerTurn);
-      } else if (!isInputOutsideOfPositionsboard(position)) {
-        play(xTurn, computerTurn);
-      } else if (!istheSpotFree(position)) {
-        play(xTurn, computerTurn);
-      } else {
-        markXonBoard(position);
-        xTurn = false;
-        computerTurn = true;
-        render(board);
-        if (getWinner()) {
-          return readline.close();
-        }
-
-        play(xTurn, computerTurn);
-      }
+const getPlayerInput = (question) => {
+  return new Promise((resolve, reject) => {
+    readline.question(question, (answer) => {
+      resolve(answer);
     });
-  }
+  });
+};
 
-  if (computerTurn) {
-    mark0onBoard();
-    xTurn = true;
-    computerTurn = false;
-    render(board);
-    if (getWinner()) {
+let player = "X";
+
+const gamePlay = async (player) => {
+  let position;
+  while (!board.gameOver()) {
+    if (player === "X") {
+      position = await getPlayerInput(`What's  your  move X?`);
+      position = parseInt(position);
+    } else {
+      position = getRandomInt();
+      console.log(`Computer moved to position ${position}`);
+    }
+    try {
+      board.write(position, player);
+      if (player === "X") {
+        player = "0";
+      } else {
+        player = "X";
+      }
+      l;
+    } catch (error) {
+      if (player === "X") {
+        gameUI.displayErrorMessage(error);
+      }
+    }
+    gameUI.renderBoard();
+    if (board.gameOver()) {
+      gameUI.displayWinner();
       return readline.close();
     }
-    play(xTurn, computerTurn);
   }
-}
+};
 
-function isInputOutsideOfPositionsboard(position) {
-  if (position < 1 || position > 9) {
-    console.log("Number should be between 1-9");
-    return false;
-  }
-  return true;
-}
-
-function isInputCorrect(position) {
-  if (Number.isNaN(position)) {
-    console.log("Input incorrect. Please make sure you add a number from 1-9.");
-    return false;
-  }
-  return true;
-}
-
-function istheSpotFree(position) {
-  if (board[position - 1] == "0" || board[position - 1] == "X") {
-    console.log("Spot is ocuppied. Try again please!");
-    return false;
-  }
-  return true;
-}
-
-function markXonBoard(position) {
-  let idx = position - 1;
-  board[idx] = "X";
-}
-
-function mark0onBoard() {
-  let indexForZero = getRandomInt();
-  if (board[indexForZero - 1] == "") {
-    console.log("Computer turn");
-    board[indexForZero - 1] = "0";
-  } else {
-    mark0onBoard();
-  }
-}
+gameUI.initialize();
+gamePlay(player);
 
 function getRandomInt() {
   min = 1;
   max = 10;
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-function getWinner() {
-  let winner = null;
-  winningCombos.forEach((combo, index) => {
-    const firstWinningPosition = combo[0];
-    const secondWinningPosition = combo[1];
-    const thirdWinningPosition = combo[2];
-    if (
-      board[firstWinningPosition] !== "" &&
-      board[firstWinningPosition] === board[secondWinningPosition] &&
-      board[firstWinningPosition] === board[thirdWinningPosition]
-    ) {
-      winner = board[combo[0]];
-    }
-  });
-  if (displayWinner(winner) === "tie") {
-    console.log("You have a tie!");
-    return "tie";
-  }
-  return winner;
-}
-
-function displayWinner(winner) {
-  if (winner === null && !board.includes("")) {
-    return "tie";
-  } else if (winner === "X") {
-    console.log("X wins");
-  } else if (winner === "0") {
-    console.log("0 wins");
-  }
-  return winner;
 }
